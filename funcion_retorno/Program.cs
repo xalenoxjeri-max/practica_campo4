ef744b2c9fccf07737374bc2a7c2d4c518eccb48
yalexiau <yalexiau@LAPTOP-EH019DKD>
2025-11-15 16:47:31 -0600

    Agregar archivos de proyecto.

---
+++ funcion_retorno/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static double CalcularArea(double baseRect, double altura)
+    {
+        return baseRect * altura;
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingrese la base del rectángulo: ");
+        double baseRect = double.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese la altura del rectángulo: ");
+        double altura = double.Parse(Console.ReadLine());
+
+        double area = CalcularArea(baseRect, altura);
+
+        Console.WriteLine("El área del rectángulo es: " + area);
+    }
+}
+
